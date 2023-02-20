379a43a0221e0e4efe5b80fd863c3acdb2bb3643
Nessie04 <insllopis@gmail.com>
2023-02-20 12:40:21 +0100

    comentarios javadoc

--- EntradaBlogILM.java
+++ EntradaBlogILM.java
@@ -1,58 +1,48 @@
-/*Clase para manejar las entradas de un blog.
- * La clase se creó el 21/3/1999 por Ana López.
- * Esta es la versión 2.3
- */
-//Modificar.
+/*@author Ines Llopis Martinez
+ * fecha 20/02
+ * @param recibe datos de entrada de un articulo
+ * @return datos del articulo
+*/
 public class EntradaBlogILM {
-//separador es el ccarácter que separa ENTRADA DE del
-//nombre del autor
 
-public static char separador=':';
-private int id;
-private String texto;
-private String autor;
+        public static char separador = ':';
+        private int id;
+        private String texto;
+        private String autor;
 
-//Constructor de la clase. recibe el número de entrada, el nombre del autor
-//de la entrada y el texto que contiene la entrada. Si el número de entrada
-//es negativo, lanza una excepción.
-public EntradaBlogILM(int id,String autor,String texto)throws IllegalArgumentException{
-        if(id<=0) throw new IllegalArgumentException("El id no puede ser negativo");
-        this.id=id;
-        this.autor=autor;
-        this.texto=texto;
-        }
-@Override
-public String toString(){
-        String cad="";
-        cad+="\nENTRADA DE"+separador+autor;
-        cad+="\n "+texto;
-        return cad;
+        public EntradaBlogILM(int id, String autor, String texto) throws IllegalArgumentException {
+                if (id <= 0) throw new IllegalArgumentException("El id no puede ser negativo");
+                this.id = id;
+                this.autor = autor;
+                this.texto = texto;
         }
 
-//Devuelve el número de la entrada
-public int getId(){
-        return this.id;
+        @Override
+        public String toString() {
+                String cad = "";
+                cad += "\nENTRADA DE" + separador + autor;
+                cad += "\n " + texto;
+                return cad;
         }
 
-//devuelve el texto completo de la entrada
-public String getTexto(){
-        return this.texto;
+        public int getId() {
+                return this.id;
         }
 
-//devuelve el nombre del autor de la entrada en mayúsculas
-public String getAutor(){
-        return this.autor.toUpperCase();
+        public String getTexto() {
+                return this.texto;
         }
 
-//devuelve el nombre del autor. Ya no se usa. Mejor ver getAutor
-public String devuelveAutor(){
-        return this.autor;
+        public String getAutor() {
+                return this.autor.toUpperCase();
         }
 
-//No tiene porqué tener argumentos.
-public static void main(String[] args) {
-        //Modificar.
-        EntradaBlogILM e1=new EntradaBlogILM (-3,"ana","Últimas noticias, está disponible BixBy 2.0");
-        System.out.println(e1);
+        public String devuelveAutor() {
+                return this.autor;
         }
+
+        public static void main(String[] args) {
+                EntradaBlogILM e1 = new EntradaBlogILM(3, "ana", "Últimas noticias, está disponible BixBy 2.0");
+                System.out.println(e1);
         }
+}
